35b4f51791b5625b9698f590813fadb1a7206395
Didy KUPANHY <d.kupanhy@gmail.com>
2020-10-30 00:52:12 +0900

    add savedir path

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,21 @@ use std::io::{BufWriter, Write};
 use fetch_market_and_order_data::stream_api::{BfWebsocket, Common, Execution, MarketInfo};
 use fetch_market_and_order_data::ohlcv::{CandleStick, Periods};
 
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "fetch_market_and_order_data")]
+struct Opt {
+    #[structopt(short, long, default_value("."))]
+    output_dir: PathBuf,
+}
+
 fn main() {
+    // コマンドライン引数から配信データ保存先を取得
+    let opt = Opt::from_args();
+    let output_dir = &opt.output_dir.display();
+
     // BitFlyerのストリーミングAPIに接続する
     let bf = BfWebsocket::new();
     bf.on_connect();
@@ -32,13 +46,13 @@ fn main() {
             // 約定データを受信した場合
             MarketInfo::Executions(execution) => {
                 // CSVに日時、売買種別、価格、注文量を書き込む
-                // 書き込み先は[{取引所}/{約定データの日付}/{約定データのチャンネル}.csv]
-                let dir_all_name = format!("{}/{}", exchange_name, execution.get_date());
+                // 書き込み先は[{指定ディレクトリ}/{取引所}/{約定データの日付}/{約定データのチャンネル}.csv]
+                let dir_all_name = format!("{}/{}/{}", output_dir, exchange_name, execution.get_date());
                 let file_name = execution.get_channel();
                 append_csv(&dir_all_name, &file_name, execution.get_csv().as_bytes());
 
                 // OHCLVデータを更新とCSVに書き込む
-                // 書き込み先は[{取引所}/{約定データの日付}/{時間足}_{約定データのチャンネル}_.csv]
+                // 書き込み先は[{指定ディレクトリ}/{取引所}/{約定データの日付}/{時間足}_{約定データのチャンネル}.csv]
                 for i in 0..candle_sticks.len() {
                     update_ohlcv_and_append_csv(&exchange_name, &mut candle_sticks[i], &execution);
                 }
@@ -46,16 +60,16 @@ fn main() {
             // 遅延データを受信した場合
             MarketInfo::LatencyExchange(latency) => {
                 // CSVに遅延時間を書き込む
-                // 書き込み先は[{取引所}/{遅延データの日付}/{遅延データのチャンネル}.csv]
-                let dir_all_name = format!("{}/{}", exchange_name, latency.get_date());
+                // 書き込み先は[{指定ディレクトリ}/{取引所}/{遅延データの日付}/{遅延データのチャンネル}.csv]
+                let dir_all_name = format!("{}/{}/{}", output_dir, exchange_name, latency.get_date());
                 let file_name = format!("latency_{}", latency.get_channel());
                 append_csv(&dir_all_name, &file_name, latency.get_csv().as_bytes());
             }
             // 板データを受信した場合
             MarketInfo::Boards(board) => {
                 // CSVに遅延時間を書き込む
-                // 書き込み先は[{取引所}/{板データの日付}/{板データのチャンネル}.csv]
-                let dir_all_name = format!("{}/{}", exchange_name, board.get_date());
+                // 書き込み先は[{指定ディレクトリ}/{取引所}/{板データの日付}/{板データのチャンネル}.csv]
+                let dir_all_name = format!("{}/{}/{}", output_dir, exchange_name, board.get_date());
                 let file_name = format!("board_{}", board.get_channel());
                 append_csv(&dir_all_name, &file_name, board.get_csv().as_bytes());
             }
